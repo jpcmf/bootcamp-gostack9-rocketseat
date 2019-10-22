d73d83c5879f041d83d533ea34f30a73eb41125f
João Paulo <musicjoaopaulo@gmail.com>
2019-10-22 14:29:19 -0300

    Add challenge 02 - Gympoint

--- challenge02/src/routes.js
+++ challenge02/src/routes.js
@@ -1,7 +1,23 @@
 import { Router } from 'express';
 
+// import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello world!!!' }));
+// routes.get('/', async (req, res) => {
+//   const user = await User.create({
+//     name: 'Joao Paulo',
+//     email: 'jpfricks@gmail.com',
+//     password_hash: '123456',
+//   });
+//   // res.json({ message: 'Hello world!!!' });
+//   return res.json(user);
+// });
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
