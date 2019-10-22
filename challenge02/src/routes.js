09ba5b435f28b5d7a8721beab4f65e52c12b4c9b
João Paulo <musicjoaopaulo@gmail.com>
2019-10-22 16:30:02 -0300

    Add challenge 02 - Gympoint

--- challenge02/src/routes.js
+++ challenge02/src/routes.js
@@ -5,6 +5,8 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 // routes.get('/', async (req, res) => {
@@ -19,5 +21,8 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
