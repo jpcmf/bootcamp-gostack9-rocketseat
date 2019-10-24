6964758b055734e044b310b9e8917467f60f38f4
João Paulo <musicjoaopaulo@gmail.com>
2019-10-23 22:57:29 -0300

    Add challenge 02 - Gympoint

---
+++ challenge02/src/app/controllers/StudentController.js
@@ -0,0 +1,19 @@
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const { id, name, email, age, weight, height } = await Student.create(
+      req.body
+    );
+    return res.json({
+      id,
+      name,
+      email,
+      age,
+      weight,
+      height,
+    });
+  }
+}
+
+export default new StudentController();
--- challenge02/src/routes.js
+++ challenge02/src/routes.js
@@ -4,6 +4,7 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -21,6 +22,7 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.post('/students', StudentController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
