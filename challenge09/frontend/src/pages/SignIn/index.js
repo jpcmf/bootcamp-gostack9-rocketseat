099675043345f63cdd17a0db93b01f28c728bb7f
João Paulo <musicjoaopaulo@gmail.com>
2020-01-02 19:33:33 -0300

    Add unform to SignIn [frontend]

--- challenge09/frontend/src/pages/SignIn/index.js
+++ challenge09/frontend/src/pages/SignIn/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+
 import logo from '~/assets/logo-gympoint@2x.png';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Gympoint" />
-      <form>
+      <Form onSubmit={handleSubmit}>
         <div className="form__group">
           <label htmlFor="email">SEU E-MAIL</label>
-          <input type="email" id="email" placeholder="exemplo@email.com" />
+          <Input name="email" type="email" placeholder="exemplo@email.com" />
         </div>
         <div className="form__group">
-          <label htmlFor="">SUA SENHA</label>
-          <input type="password" placeholder="******" />
+          <label htmlFor="password">SUA SENHA</label>
+          <Input name="password" type="password" placeholder="*************" />
         </div>
         <button type="submit">Entrar no sistema</button>
-      </form>
+      </Form>
     </>
   );
 }
