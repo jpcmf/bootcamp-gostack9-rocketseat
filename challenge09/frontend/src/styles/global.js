0002ad868715ad4151d1e1a9b72cf899cc1d27ae
João Paulo <musicjoaopaulo@gmail.com>
2020-01-02 14:31:37 -0300

    Add global styles [frontend]

---
+++ challenge09/frontend/src/styles/global.js
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+// import 'react-toastify/dist/ReactToastify.css';
+// import 'react-perfect-scrollbar/dist/css/styles.css';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  body, input, button {
+    color: #222;
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
