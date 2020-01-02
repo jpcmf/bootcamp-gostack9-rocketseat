ab02b4dc8fdc88dccdc5d1e831bb85869f8dd59b
João Paulo <musicjoaopaulo@gmail.com>
2020-01-02 20:50:37 -0300

    Add authentication to SignIn [frontend]

--- challenge09/frontend/src/store/modules/auth/sagas.js
+++ challenge09/frontend/src/store/modules/auth/sagas.js
@@ -1,3 +1,30 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password, permission_level } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+    permission_level,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.permission_level === 'admin') {
+    console.tron.error('Usuário não tem nível de administrador.');
+
+    return;
+  }
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
