3cf17f0593e900e9e27eec8e43f2d2e3e8e3c5cf
João Paulo <musicjoaopaulo@gmail.com>
2020-01-03 11:26:13 -0300

    Add styles to the Header component [frontend]

---
+++ challenge09/frontend/src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo2-gympoint@2x.png';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="Gympoint" />
+          <Link to="/">ALUNOS</Link>
+          <Link to="/">PLANOS</Link>
+          <Link to="/">MATRÍCULAS</Link>
+          <Link to="/">PEDIDOS DE AUXÍLIO</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>João Paulo Fricks</strong>
+              <Link to="/logout">sair do sistema</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/40/abott@adorable.png"
+              alt="João Paulo Fricks"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
