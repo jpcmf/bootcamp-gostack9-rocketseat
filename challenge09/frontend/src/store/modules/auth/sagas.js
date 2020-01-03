430d43747eeb1665dff59bac6fdcc2088943c5ba
João Paulo <musicjoaopaulo@gmail.com>
2020-01-02 21:25:31 -0300

    Add loading to SignIn [frontend]

--- challenge09/frontend/src/store/modules/auth/sagas.js
+++ challenge09/frontend/src/store/modules/auth/sagas.js
@@ -3,28 +3,32 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password, permission_level } = payload;
+  try {
+    const { email, password, permission_level } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-    permission_level,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+      permission_level,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.permission_level === 'admin') {
-    console.tron.error('Usuário não tem nível de administrador.');
+    if (!user.permission_level === 'admin') {
+      console.tron.error('Usuário não tem nível de administrador.');
 
-    return;
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
